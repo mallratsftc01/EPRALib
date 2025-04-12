b90fdfb0520c5dbf9d1cfe4b2c21b016496af427
mallratsftc01 <mallratsftc01@gmail.com>
2025-04-12 13:43:13 -0400

    4/12A/2025
    - Updated the build.gradle file
    - Created a new module, FTCLib, to hold all the files for the EPRA FTC library
    - Published this module as a GitHub Package

---
+++ ftclib/src/main/java/location/Section.java
@@ -0,0 +1,3 @@
+package location;
+
+public interface Section {}
---
+++ ftclib/src/main/java/math/geometry/Shape2D.java
@@ -0,0 +1,8 @@
+package math.geometry;
+
+public interface Shape2D {
+
+    public double getArea();
+
+    public boolean checkPoint(Point point);
+}
